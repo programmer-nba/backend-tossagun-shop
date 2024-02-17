8375212f7d13cd6b0ea80fe22e343252219f0098
Programmer NBA <137274609+programmer-nba@users.noreply.github.com>
2024-02-17 15:23:41 +0700

    Update admin.js

--- routes/user/admin.js
+++ routes/user/admin.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const admins = require("../../controllers/user/admin.controller");
 const authAdmin = require("../../lib/auth.admin");
 
-router.post("/", admins.create);
+router.post("/", authAdmin, admins.create);
 
 router.get("/", authAdmin, admins.findAll);
 router.get("/:id", authAdmin, admins.findOne);
