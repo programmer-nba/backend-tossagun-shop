c00cdc15b75f5012f04519f9093413f685c4998c
Programmer NBA <137274609+programmer-nba@users.noreply.github.com>
2024-02-17 17:45:07 +0700

    Update platform.js

--- routes/user/platform.js
+++ routes/user/platform.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const platform = require("../../controllers/user/platform.controller");
 const authAdmin = require("../../lib/auth.admin");
 
-router.post("/checkMember/:tel", platform.checkMember);
+router.get("/checkMember/:tel", platform.checkMember);
 router.post("/register", platform.register);
 router.post("/teammember/:tel", platform.getTeammember);
 router.post("/member/all", authAdmin, platform.getMemberAll);
