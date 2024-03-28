c88b9b16c7402b5e15bafaf90299edb908d8c17b
Programmer NBA <programmer@nbadigitalworlds.com>
2024-03-28 17:16:45 +0700

    Update price.model.js

--- model/service/artwork/price.model.js
+++ model/service/artwork/price.model.js
@@ -3,10 +3,20 @@ const Joi = require("joi");
 
 const PriceArtworkShema = new mongoose.Schema({
     product_id: { type: String, required: true },
+    shop: {
+        profit: { type: Number, required: true },
+        platform: { type: Number, required: true },
+    },
+    service: {
+        profit: { type: Number, required: true },
+        platform: { type: Number, required: true },
+    },
+    platform: {
+        profit: { type: Number, required: true },
+        platform: { type: Number, required: true },
+    },
     price: { type: Number, required: true },
     cost: { type: Number, required: true },
-    profit: { type: Number, required: true },
-    platform: { type: Number, required: true },
     freight: { type: Number, required: true },
     unit: { type: Number, required: true },
     unit_pack: { type: Number, required: true },
@@ -17,10 +27,20 @@ const PriceArtworks = mongoose.model("artwork_price", PriceArtworkShema);
 const validate = (data) => {
     const Schema = Joi.object({
         product_id: Joi.string().required().label("กรอกไอดี Product Artwork"),
+        shop: Joi.object({
+            profit: Joi.number().required().label("โปรดกรอกกำไร"),
+            platform: Joi.number().required().label("โปรดก่อนส่วนแบ่งแพลตฟอร์ม"),
+        }),
+        service: Joi.object({
+            profit: Joi.number().required().label("โปรดกรอกกำไร"),
+            platform: Joi.number().required().label("โปรดก่อนส่วนแบ่งแพลตฟอร์ม"),
+        }),
+        platform: Joi.object({
+            profit: Joi.number().required().label("โปรดกรอกกำไร"),
+            platform: Joi.number().required().label("โปรดก่อนส่วนแบ่งแพลตฟอร์ม"),
+        }),
         price: Joi.number().required().label("โปรดกรอกราคา"),
         cost: Joi.number().required().label("โปรดกรอกต้นทุน"),
-        profit: Joi.number().required().label("โปรดกรอกกำไร"),
-        platform: Joi.number().required().label("โปรดก่อนส่วนแบ่งแพลตฟอร์ม"),
         freight: Joi.number().required().label("กรอกค่าขนส่ง"),
         unit: Joi.number().required().label("กรอกจำนวนที่ขาย"),
         unit_pack: Joi.number().required().label("กรอกจำนวนแพ็ค"),
