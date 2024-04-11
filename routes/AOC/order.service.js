8e33315b28d6ff9a085ad2d7b2229971f3194903
Bew2000 <146051593+Bew2000@users.noreply.github.com>
2024-04-11 15:50:43 +0700

    11/04/2024

--- routes/AOC/order.service.js
+++ routes/AOC/order.service.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const aoc = require("../../controllers/AOC/aoc.order.service.controller");
 const auth = require("../../lib/auth");
 
-router.get("/get/ticket",auth, aoc.getFlightTicket);
-router.get("/get/price", auth, aoc.getPriceTicket);
+router.post("/get/ticket",auth, aoc.getFlightTicket);
+router.post("/get/price", auth, aoc.getPriceTicket);
 router.post("/booking", auth, aoc.getBooking);
-router.get("/get/flight/booking", auth, aoc.getFlightBooking);
-// router.put("/update/payment", auth, aoc.updatePayment);
-router.put("/confirm/:id", auth, aoc.confirmAOC);
+router.post("/get/flight/booking", auth, aoc.getFlightBooking);
+router.post("/update/payment", auth, aoc.updatePayment);
+router.post("/confirm/:id", auth, aoc.confirmAOC);
 // router.get("/store-id/:store_id", auth, order.findByStoreId);
 // router.get("/ponba/:ponba_id", auth, order.findByPoNbaId);
 
