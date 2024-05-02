c07a35a76d1806b739aa72521409f3697112603d
Programmer NBA <programmer@nbadigitalworlds.com>
2024-05-02 14:43:21 +0700

    up

--- model/shippop/customer.model.js
+++ model/shippop/customer.model.js
@@ -4,16 +4,16 @@ const Schema = mongoose.Schema
 //ผู้ส่งสินค้า
 const customerSchema = new Schema({
     shop_id: { type: String, required: true },
-    name: {type:String, required: false},
-    email: {type:String, default:"", required: false},
-    address:{type:String, required: false},
-    district:{type:String, required: false,},
-    state:{type:String, required: false},
-    province:{type:String, required: false},
-    postcode:{type:String, required: true},
-    tel:{type:String, required: true},
-    status:{type:String, required:false},
-},{timestamps: true});
+    name: { type: String, required: false },
+    email: { type: String, default: "", required: false },
+    address: { type: String, required: false },
+    district: { type: String, required: false, }, //ตำบล
+    state: { type: String, required: false }, //อำเภอ
+    province: { type: String, required: false }, //จังหวัด
+    postcode: { type: String, required: true },
+    tel: { type: String, required: true },
+    status: { type: String, required: false },
+}, { timestamps: true });
 
 const customerShippop = mongoose.model("customer_shippop", customerSchema);
 
