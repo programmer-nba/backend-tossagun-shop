5cd15984b4a22b4e7eb30b1270dd1f02092affd2
Programmer NBA <programmer@nbadigitalworlds.com>
2024-06-06 17:29:17 +0700

    up

--- controllers/topup/topup.service.controller.js
+++ controllers/topup/topup.service.controller.js
@@ -3,6 +3,7 @@ const { PercentTopup } = require("../../model/topup/topup.percent");
 module.exports.booking = async (req, res) => {
     try {
         const percent = await PercentTopup.find();
+        console.log(percent)
     } catch (error) {
         console.log(error);
         return res.status(500).send({ message: "เกิดข้อผิดพลาดบางอย่าง", data: error.data });
