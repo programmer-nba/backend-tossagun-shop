a5bda7b184518aabb05e62fff0f3860ed9ebb265
Programmer NBA <programmer@nbadigitalworlds.com>
2024-07-26 16:54:32 +0700

    Update index.js

--- routes/contract/index.js
+++ routes/contract/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const contract = require("../../controllers/contract/contract.controller");
 
 router.post("/PDPA", contract.getContractPDPA);
-router.post("/TSGWEB", contract.getContractCondition);
+router.post("/RENT-WEB", contract.getContractCondition);
 
 module.exports = router;
