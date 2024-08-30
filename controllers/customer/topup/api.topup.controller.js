9df84732a0514c25ca642dfdfea48a92c5cacc9a
Programmer NBA <programmer@nbadigitalworlds.com>
2024-08-30 16:11:42 +0700

    up

--- controllers/customer/topup/api.topup.controller.js
+++ controllers/customer/topup/api.topup.controller.js
@@ -1,13 +1,32 @@
+const dayjs = require("dayjs");
 const { OrderTopup } = require("../../../model/topup/order.topup");
 const { PercentTopup } = require("../../../model/topup/topup.percent");
+const { AWSBooking } = require("../../../model/topup/aws.order");
+const { Customers } = require("../../../model/user/customer.model");
 
 module.exports.booking = async (req, res) => {
 	try {
+		const id = req.decoded.id;
+
+		// ค้นหาลูกค้า
+		const findCustomer = await Customers.findOne({ _id: id });
+		if (!findCustomer) {
+			return res
+				.status(404)
+				.send({ status: false, message: "ไม่พบข้อมูลลูกค้าดังกล่าว" })
+		}
+
+		const amount = req.body.reduce((sum, el) => sum + el.amount, 0);
+		if (findCustomer.cus_wallet < amount) {
+			return res.status(405).send({ status: false, message: 'ยอดเงินไม่เพียงพอต่อการทำรายการ' })
+		}
+
 		const invoice = await invoiceNumber();
 		const ref_number = await runreference_order();
-
-		for (let item of req.body.product_detail) {
-			const percent = await PercentTopup.findOne({ topup_id: item.service_id });
+		const new_data = [];
+		let total = 0;
+		for (let item of req.body) {
+			// const percent = await PercentTopup.findOne({ topup_id: item.service_id });
 
 			const value = {
 				reference_order: ref_number,
@@ -18,14 +37,43 @@ module.exports.booking = async (req, res) => {
 				ref1: invoice,
 			};
 
-			const resp = await axios.post(`${process.env.TOPUP_URL}`, value, {
-				headers: {
-					"Content-Type": "application/json",
-					"Authorization": `Bearer ${process.env.TOPUP_TOKEN}`
-				},
-			});
+			// const resp = await axios.post(`${process.env.TOPUP_URL}`, value, {
+			// headers: {
+			// "Content-Type": "application/json",
+			// "Authorization": `Bearer ${process.env.TOPUP_TOKEN}`
+			// },
+			// });
+
+			// console.log(res)
+
+			const v = {
+				...item,
+				invoice: invoice,
+				ref_number: ref_number,
+				shop_id: id,
+				// employee_id: req.body.employee,
+				// type_payment: req.body.paymenttype,
+				// tossagun_tel: req.body.platform,
+				// order_id: resp.data.order_id,
+				// order_status: resp.data.status,
+				timestamp: dayjs(Date.now()).format(),
+			};
+			new_data.push(v);
+			total += item.amount;
 		}
 
+		const wallet = findCustomer.cus_wallet - total;
+		await Customers.findByIdAndUpdate(id, { cus_wallet: wallet }, { useFindAndModify: false }).then((data) => {
+			if (!data) {
+				return res.status(405).send({ status: false, message: 'ตัดเงินจากกระเป๋าไม่สำเร็จ' })
+			}
+		});
+
+		const createOrder = await AWSBooking.insertMany(new_data);
+		if (!createOrder) {
+			console.log("ไม่สามารถสร้างข้อมูล booking ได้")
+		}
+		return res.status(200).send({ status: true, data: new_data, ref: ref_number })
 	} catch (error) {
 		console.log(error)
 		return res.status(500).send({ message: "Internal Server Error" })
