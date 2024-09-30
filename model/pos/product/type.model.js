6b4490cb53fd34ff3501bacba7de1bb359c0cda5
Programmer NBA <programmer@nbadigitalworlds.com>
2024-09-30 19:05:49 +0700

    Update type.model.js

--- model/pos/product/type.model.js
+++ model/pos/product/type.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Joi = require("joi");
 
 const TypeSchema = new mongoose.Schema({
+	category_id: { type: String, required: true },
 	name: { type: String, required: true },
 });
 
@@ -9,6 +10,7 @@ const Types = mongoose.model("type", TypeSchema);
 
 const validate = (data) => {
 	const schema = Joi.object({
+		category_id: Joi.string().required().label("กรุณากรอกไอดีประเภทสินค้า"),
 		name: Joi.string().required().label("กรุณากรอกชื่อประเภทย่อย"),
 	});
 	return schema.validate(data);
